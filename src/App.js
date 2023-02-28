f5b5a4f18b513363a3c4f1f203632a381b181ba6
Raidro <mateus_sat@hotmail.com>
2023-02-28 15:45:10 -0300

    adiciona o leaftleat ao sistema e cria o appbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import "./App.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <text>OK</text>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            style={{ color: "black" }}
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            Gestão de pontos no mapa
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
