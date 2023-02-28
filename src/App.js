f3468aefa07c1a1f1a14b11ae5f4873719262eb1
Raidro <mateus_sat@hotmail.com>
2023-02-28 17:27:34 -0300

    add o json de talhao e encontra o centro do poligono

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import * as React from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import {
+  MapContainer,
+  useMapEvents,
+  TileLayer,
+  Popup,
+  Marker,
+} from "react-leaflet";
+import { GeoJSON } from "react-leaflet";
 
 import "./App.css";
 
+import GeoData from "./JSON/Talhao.json";
+
 function App() {
+  const [lntlng, setLntLng] = useState([
+    -15.179037392360357, -53.58474565423604,
+  ]);
+
+  // encontra o ponto central
+
+  function MyComponent() {
+    const map = useMapEvents({
+      click: (e) => {
+        const { lat, lng } = e.latlng;
+        console.log("Longitude => ", lng);
+        console.log("Latitude => ", lat);
+      },
+    });
+    return null;
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="transparent">
@@ -23,22 +50,23 @@ function App() {
         </Toolbar>
       </AppBar>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={lntlng}
+        zoom={15}
         scrollWheelZoom={false}
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "100vw", height: "80vh" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={lntlng}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <GeoJSON key="my-geojson" data={GeoData} />
+        <MyComponent />
       </MapContainer>
-      //{" "}
     </Box>
   );
 }
