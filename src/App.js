9d6c432b7a02127c78229bde1fba7d473edda52f
Raidro <mateus_sat@hotmail.com>
2023-03-05 21:20:26 -0300

    add satellite view

--- src/App.js
+++ src/App.js
@@ -108,6 +108,12 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <TileLayer
+            url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+            maxZoom={20}
+            subdomains={["mt1", "mt2", "mt3"]}
+          />
+
           <Marker
             draggable
             position={lntlng}
